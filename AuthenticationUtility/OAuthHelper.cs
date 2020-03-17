302abc9c2991cfaee003fde1312a8bebe345b85b
Paul Wu (MBS) <qipengwu@microsoft.com>
2020-03-17 12:38:15 -0700

    Test upstream parent

--- AuthenticationUtility/OAuthHelper.cs
+++ AuthenticationUtility/OAuthHelper.cs
@@ -27,7 +27,7 @@ namespace AuthenticationUtility
 
             AuthenticationContext authenticationContext = new AuthenticationContext(aadTenant);
 
-            // OAuth through username and password.
+            // OAuth through username and password. test git upstream merge from parent.
             string username = ClientConfiguration.Default.UserName;
             string password = ClientConfiguration.Default.Password;
 
